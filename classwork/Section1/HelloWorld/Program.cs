47ac32d73d16a5faf1b43f4bc1a4cfbe47ef6917
Peter <peter.young068@my.tccd.edu>
2019-08-26 18:55:56 -0500

    classwork 2

--- classwork/Section1/HelloWorld/Program.cs
+++ classwork/Section1/HelloWorld/Program.cs
@@ -4,9 +4,13 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main ( string[] args )
+        static void Main ( /*string[] args*/ )
         {
-
+            String name;
+            name = "bob";
+            String name2;
+            name2 = Console.ReadLine ();
+            Console.WriteLine (name2);
             Console.WriteLine ("Hello World!");
         }
     }
