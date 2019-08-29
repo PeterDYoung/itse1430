46893cb4c146bd28a6f4a677eed11612a5686e30
Peter <peter.young068@my.tccd.edu>
2019-08-29 14:15:17 -0500

    Initial Lab1

---
+++ Lab1/Itse1430.Maze/Maze.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Itse1430.Maze
+{
+    class Maze
+    {
+        Room currRoom;
+        bool faceNorth;
+        bool faceEast;
+        bool faceSouth;
+        bool faceWest;
+        Room endRoom;
+        void Move ( string direction )
+        {
+
+        }
+        void Turn ( string direction )
+        {
+
+        }
+        void Examine ()
+        {
+
+        }
+    }
+}
---
+++ Lab1/Itse1430.Maze/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Itse1430.Maze
+{
+    class Room{
+        Tuple<int, int> coord;
+        bool northDoor;
+        bool eastDoor;
+        bool southDoor;
+        bool westDoor;
+
+        bool canTraverse( Room destinationRoom) {
+            return false;
+        }
+    }
+}
